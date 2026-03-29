d67b6550f0702f3744b03e505aac8699b05baee6
Edmilson <45573538+Edmilson-Hao@users.noreply.github.com>
2026-03-28 22:12:19 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'mobilidade-ed-v1';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
