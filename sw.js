4c48297db130cf36a75044cd5fc22ec15411907d
Edmilson <45573538+Edmilson-Hao@users.noreply.github.com>
2026-03-28 22:43:26 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,22 @@
-const CACHE_NAME = 'mobilidade-v1';
+const CACHE_NAME = 'mob-ed-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-// Instala e força o SW a tomar controle imediatamente
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
-});
-
-// Responde às requisições (necessário para o status de PWA)
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match(event.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
